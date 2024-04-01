defd3e29cdc5f95531b0beb2cd0d59bc8aa17cf7
saranshg20 <saranshg20@iitk.ac.in>
2024-04-01 12:08:08 +0530

    Add initial project files

---
+++ style.js
@@ -0,0 +1,26 @@
+import { StyleSheet } from "react-native";
+
+export const styles = StyleSheet.create({
+  button: {
+    alignItems: "center",
+    justifyContent: "center",
+    paddingVertical: 12,
+    paddingHorizontal: 32,
+    borderRadius: 4,
+    elevation: 3,
+    backgroundColor: "black",
+  },
+  container: {
+    flex: 1,
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  text: {
+    fontSize: 16,
+    lineHeight: 21,
+    fontWeight: "bold",
+    letterSpacing: 0.25,
+    color: "white",
+  },
+});
